5775171a5cb776089ec1de0c27e44074c79d73c0
Jeremias Kangas <jeremias.kangas@gmail.com>
2016-10-17 09:37:34 +0300

    initial

--- main.py
+++ main.py
@@ -5,5 +5,5 @@ class AddBloomFilter(object):
 
     def __init__(self, max_size_exponent=20, hash_functions=3):
         super(AddBloomFilter, self).__init__()
-        self.data = b'\x00' * 2^^max_size_exponent
+        self.data = bytearray(2^^max_size_exponent)
 
